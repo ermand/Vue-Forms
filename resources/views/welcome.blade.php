053f7cf371da2fa8b2e6d5936ab1bf4dc847a8ec
Ermand Durro <ermand.duro@gmail.com>
2017-03-31 11:26:45 +0200

    Add bulma CSS framework

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
 
         <title>Laravel</title>
     </head>
     <body>
-         <div id="app">
-            <ul>
-                <li v-for="skill in skills" v-text="skill"></li>
-            </ul>
-             
-         </div>
+        <div class="container">
+            <div id="app" class="content">
+                <h2>Skills</h2>
+                <ul>
+                    <li v-for="skill in skills" v-text="skill"></li>
+                </ul>
+            </div>
+        </div>
 
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-         <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-         <script src="/js/app.js"></script>
-     </body> 
+        <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
+        <script src="/js/app.js"></script>
+     </body>
 </html>
